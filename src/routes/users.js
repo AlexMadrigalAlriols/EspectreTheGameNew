4b6faae727451831142b8edc06ecf2fac85fecef
Alex Madrigal Alriols <73166221+AlexMadrigalAlriols@users.noreply.github.com>
2021-01-12 16:45:41 +0100

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -222,7 +222,7 @@ router.get('/ingame', isAuthenticated, async (req, res) =>{
         var game = await Game.findById('5fedf15fa1268c39d8229e47');
             if(group.Ataqued == true){
               req.flash('error_msg', 'Estas siendo atacado usa una carta de defensa para defenderte!');
-              res.render('layouts/mapa.hbs', { game, user, group });
+              res.redirect('/ingame/cards/');
            }else{
               res.render('layouts/mapa.hbs', { game, user, group });
            }
@@ -231,7 +231,7 @@ router.get('/ingame', isAuthenticated, async (req, res) =>{
         var group = await Group.findOne({name: user.group + 'T'});
         if(group.Ataqued == true){
               req.flash('error_msg', 'Estas siendo atacado usa una carta de defensa para defenderte!');
-              res.render('layouts/mapa.hbs', { game, user, group });
+              res.redirect('/ingame/cards/');
            }else{
               res.render('layouts/mapa.hbs', { game, user, group });
            }
