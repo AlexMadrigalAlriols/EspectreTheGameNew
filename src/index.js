9f6bd81f035cdb9199b5ba9ebdb0694b24a62536
Alex Madrigal Alriols <73166221+AlexMadrigalAlriols@users.noreply.github.com>
2021-01-12 10:23:51 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const { v4: uuidv4 } = require('uuid');
 
 
 const storage = multer.diskStorage({
-    destination: path.join('/home/espectre/web/uploads'),
+    destination: path.join(__dirname + '/public/uploads'),
     filename: (req, file, cb, filename) => {
         cb(null, uuidv4() + path.extname(file.originalname));
     }
